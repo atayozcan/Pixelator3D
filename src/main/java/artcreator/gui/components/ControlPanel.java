3260e8cc77b3614ef5de267a38a90fc3c6404b8c
Atay Özcan <atayozcan@pm.me>
2025-12-18 15:10:25 +0100

    Slider-Wertanzeige deaktiviert, nur Label daneben

--- src/main/java/artcreator/gui/components/ControlPanel.java
+++ src/main/java/artcreator/gui/components/ControlPanel.java
@@ -24,11 +24,13 @@ public class ControlPanel {
 
         // Row 1: Load + Pixel Size Slider
         var row1 = new JPanel(new FlowLayout(FlowLayout.CENTER, 15, 5));
-        row1.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
         row1.add(UIConfig.button("Load Image", onLoad));
 
         row1.add(new JLabel("Pixel Size:"));
         pixelSizeSlider = new JSlider(2, 50, 10);
+        pixelSizeSlider.setPaintTrack(true);
+        pixelSizeSlider.setPaintTicks(false);
+        pixelSizeSlider.setPaintLabels(false);
         pixelSizeSlider.setPreferredSize(new Dimension(120, 20));
         pixelSizeLabel = new JLabel("10");
         pixelSizeLabel.setFont(UIConfig.FONT_BOLD);
